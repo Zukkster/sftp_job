aba16931f9f6dd31c14528a454c65df22fc25353
Zukkster <31768337+Zukkster@users.noreply.github.com>
2020-10-29 20:25:24 +0000

    Initial commit

---
+++ send_email.py
@@ -0,0 +1,32 @@
+import smtplib
+import ssl
+
+smtp_server = "smtp.gmail.com"
+port = 465  # F0r starttls
+sender_email = "db_dev@jin-analytics.co.uk"
+receiver_email = "stuartkirkup@gmail.com"
+password = "KG52_wtt"
+
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+# Try to log in to server and send email
+try:
+    #server = smtplib.SMTP(smtp_server,port)
+    #server.ehlo() # Can be omitted
+    # server.starttls(context=context) # Secure the connection
+    # server.ehlo() # Can be omitted
+    # server.login(sender_email, password)
+    # TODO: Send email here
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+#finally:
+    #server.quit()
