9ab3067edd7f405ba747bc100fab38eeb1aedb88
Zukkster <31768337+Zukkster@users.noreply.github.com>
2020-10-31 10:52:32 +0000

    Fix keyfile_path

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ password = os.environ['DIVENDO_SFTP_PASSWORD']
 username = os.environ['DIVENDO_SFTP_USERNAME']
 local_path = '/'
 remote_path = '/home/db_admin/data/vivobarefoot/divendo/'
-keyfile_path=None
+#keyfile_path=None
 port = 22
 
 #Connect to SFTP site
-sftpclient = create_sftp_client2(host, port, username, password, keyfile_path, 'RSA')
+sftpclient = create_sftp_client2(host, port, username, password, None, None)
 
 #get files
 print("get file 1")
